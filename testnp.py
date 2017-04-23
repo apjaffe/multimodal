ae4407391a2855fd844ae695c4e138b9d6f1727c
Ubuntu <ubuntu@ip-172-31-36-90.us-west-2.compute.internal>
2017-04-23 18:17:38 +0000

    Alternative encdec

--- testnp.py
+++ testnp.py
@@ -1,2 +1,4 @@
 import numpy as np
-data = np.fromfile('task2_ResNet50_res4fx_test2017.fp16.npy')
+#data = np.load('task2_ResNet50_res4fx_test2017.fp16.npy')
+data = np.load('flickr30k_ResNets50_blck4_train.fp16.npy')
+
