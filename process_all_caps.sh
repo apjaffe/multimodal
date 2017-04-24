d0349769191fa22cd975cf27cb96c0d00f7d88e2
Ubuntu <ubuntu@ip-172-31-36-90.us-west-2.compute.internal>
2017-04-24 06:12:17 +0000

    Concat translation files

--- process_all_caps.sh
+++ process_all_caps.sh
@@ -1,3 +1,9 @@
+mkdir -p tokenized/en/val
+mkdir -p tokenized/en/train
+mkdir -p tokenized/de/val
+mkdir -p tokenized/de/train
+mkdir -p translate
+
 for i in `seq 1 5`;
 do
   python process_captions.py mmt_task2/en/train/train.${i} > tokenized/en/train/train.${i}
@@ -5,3 +11,9 @@ do
   python process_captions.py mmt_task2/en/val/val.${i} > tokenized/en/val/val.${i}
   python process_captions.py mmt_task2/de/val/de_val.${i} > tokenized/de/val/de_val.${i}
 done
+
+cat tokenized/en/train/train.* > translate/train.en
+cat tokenized/de/train/de_train.* > translate/train.de
+cat tokenized/en/val/val.* > translate/valid.en
+cat tokenized/de/val/de_val.* > translate/valid.de
+
